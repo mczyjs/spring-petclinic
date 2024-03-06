d8e37dd913fad2a67f75d0a62b435099bd721fb5
mczyjs <mczyjs@TeamCity>
2024-03-06 03:32:23 +0000

    TeamCity change in 'Spring Petclinic' project: Versioned settings configuration updated

---
+++ .teamcity/settings.kts
@@ -0,0 +1,78 @@
+import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.buildFeatures.jiraCloudIntegration
+import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
+import jetbrains.buildServer.configs.kotlin.buildSteps.maven
+import jetbrains.buildServer.configs.kotlin.projectFeatures.jira
+import jetbrains.buildServer.configs.kotlin.triggers.vcs
+
+/*
+The settings script is an entry point for defining a TeamCity
+project hierarchy. The script should contain a single call to the
+project() function with a Project instance or an init function as
+an argument.
+
+VcsRoots, BuildTypes, Templates, and subprojects can be
+registered inside the project using the vcsRoot(), buildType(),
+template(), and subProject() methods respectively.
+
+To debug settings scripts in command-line, run the
+
+    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
+
+command and attach your debugger to the port 8000.
+
+To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
+-> Tool Windows -> Maven Projects), find the generate task node
+(Plugins -> teamcity-configs -> teamcity-configs:generate), the
+'Debug' option is available in the context menu for the task.
+*/
+
+version = "2023.11"
+
+project {
+
+    buildType(Build)
+
+    features {
+        jira {
+            id = "PROJECT_EXT_7"
+            displayName = "Demo"
+            host = "https://xdatatech-team-a1bchh7kh5hr.atlassian.net/"
+            userName = "mazs@xdatatech.com"
+            password = "credentialsJSON:5370317a-04c6-4561-8322-26ddff69bfd0"
+            projectKeys = "SPN"
+            cloudClientID = "awNqC8oK0J7BzqcNAV1yC1BsPIPKv2NE"
+            cloudSecret = "credentialsJSON:835e9f4d-84db-44d1-8b61-6f786b126441"
+        }
+    }
+}
+
+object Build : BuildType({
+    name = "Build"
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
+
+    steps {
+        maven {
+            id = "Maven2"
+            goals = "clean test"
+            runnerArgs = "-Dmaven.test.failure.ignore=true"
+            jdkHome = "%env.JDK_17_0%"
+        }
+    }
+
+    triggers {
+        vcs {
+        }
+    }
+
+    features {
+        perfmon {
+        }
+        jiraCloudIntegration {
+            issueTrackerConnectionId = "PROJECT_EXT_7"
+        }
+    }
+})
