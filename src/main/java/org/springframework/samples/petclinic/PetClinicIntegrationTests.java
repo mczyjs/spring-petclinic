daa3099c12911aaf52d87e4d51d00cf21f95528a
mczyjs <156386743+mczyjs@users.noreply.github.com>
2024-03-11 14:29:32 +0800

    Update PetClinicIntegrationTests.java

--- src/main/java/org/springframework/samples/petclinic/PetClinicIntegrationTests.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicIntegrationTests.java
@@ -3,6 +3,8 @@ package org.springframework.samples.petclinic;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@SpringBootApplication
+@ImportRuntimeHints(PetClinicRuntimeHints.class)
 public class PetClinicIntegrationTests {
 
 	@Test
